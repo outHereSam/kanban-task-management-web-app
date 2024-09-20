365052d588f237b3c4eff90857197b9f5536584b
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-20 17:50:26 +0000

    Fixes issue where completed subtasks weren't showing

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -8,23 +8,53 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { Task } from '../../models/board.model';
 import { TaskDetailModalComponent } from '../task-detail-modal/task-detail-modal.component';
 import { MatDialog } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { selectTask } from '../../state/boards/selectors/boards.selectors';
+import { distinctUntilChanged, map, Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [TaskDetailModalComponent, MatDialogModule],
+  imports: [TaskDetailModalComponent, MatDialogModule, AsyncPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './task.component.html',
   styleUrl: './task.component.sass',
 })
 export class TaskComponent {
+  task$: Observable<Task | undefined>;
   @Input() task!: Task;
-  completedSubtaskCount: number = 0;
+  completedSubtaskCount$: Observable<number>;
+
+  // completedSubtaskCount: number = 0;
   dialog = inject(MatDialog);
 
+  constructor(private store: Store) {
+    this.task$ = this.store.select(
+      selectTask(this.task?.id, this.task?.status)
+    );
+    this.completedSubtaskCount$ = this.task$.pipe(
+      map(
+        (task) =>
+          task?.subtasks?.filter((subtask) => subtask.isCompleted).length ?? 0
+      ),
+      distinctUntilChanged()
+    );
+  }
+
   ngOnInit() {
-    this.task.subtasks.map((subtask) => {
-      if (subtask.isCompleted) this.completedSubtaskCount += 1;
-    });
+    // this.task.subtasks.map((subtask) => {
+    //   if (subtask.isCompleted) this.completedSubtaskCount += 1;
+    // });
+    this.task$ = this.store.select(
+      selectTask(this.task?.id, this.task?.status)
+    );
+    this.completedSubtaskCount$ = this.task$.pipe(
+      map(
+        (task) =>
+          task?.subtasks?.filter((subtask) => subtask.isCompleted).length ?? 0
+      ),
+      distinctUntilChanged()
+    );
   }
 
   openDialog() {
