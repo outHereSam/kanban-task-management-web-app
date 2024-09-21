c86660f20cce42486e35399ff6a8ab57a35803ab
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-21 17:50:08 +0000

    Makes add column button functional

--- src/app/components/board-content/board-content.component.ts
+++ src/app/components/board-content/board-content.component.ts
@@ -1,22 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Board } from '../../models/board.model';
 import { TaskComponent } from '../task/task.component';
 import { generateUniquePastelColor } from '../../utils/functions';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { BoardFormComponent } from '../../shared/board-form/board-form.component';
 
 @Component({
   selector: 'app-board-content',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, MatDialogModule],
   templateUrl: './board-content.component.html',
   styleUrl: './board-content.component.sass',
 })
 export class BoardContentComponent {
   @Input() board!: Board;
   colorList: string[] = [];
+  dialog = inject(MatDialog);
 
   ngOnInit() {
     this.board.columns.forEach((column) =>
       this.colorList.push(generateUniquePastelColor())
     );
+  }
+
+  openBoardForm() {
+    this.dialog.open(BoardFormComponent, {
+      width: '480px',
+      data: this.board,
+    });
   }
 }
